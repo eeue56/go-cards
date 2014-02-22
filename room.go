1503931d77fb44b439558b6e9d571c96769b74eb
eeue56 <eeue56@bangor.ac.uk>
2014-02-22 14:26:54 +0000

    adding dealer handouts

--- room.go
+++ room.go
@@ -2,12 +2,39 @@ package gocards
 
 type Room struct {
     Players     []Player
-    dealer      int
     BlackHand   []BlackCard
+    dealer      int
+}
+
+func NewRoom() *Room {
+    room := Room{}
+    room.dealer = 0
+    room.Players = []Player{}
+    room.BlackHand = []BlackCard{}
+
+    return &room
 }
 
 func (room *Room) Dealer() Player {
     return room.Players[room.dealer]
+}
+
+func (room *Room) NonDealers() chan Player {
+     c := make(chan Player)
+
+     go func(count int, players []Player, dealerNumber int) {
+        for i := 0; i < count; i++ {
+            if i == dealerNumber {
+                continue
+            }
+
+            c <- players[i]
+        }
+
+        close(c)
+     }(len(room.Players), room.Players, room.dealer)
+
+     return c
 }
 
 func (room *Room) RotateDealer() {
